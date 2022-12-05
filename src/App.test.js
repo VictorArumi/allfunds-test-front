048dca4b50ec8388a5a2ed021e695c39bc29d8f2
VictorArumi <99591372+VictorArumi@users.noreply.github.com>
2022-12-05 20:28:18 +0100

    Set github actions, sonar properties and husky with hooks

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders news app in progress", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/news app in progress/i);
   expect(linkElement).toBeInTheDocument();
 });
