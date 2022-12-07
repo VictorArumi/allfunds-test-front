5979d2801b9114f551ef7b80b03b6ff92c6d2fd6
VictorArumi <99591372+VictorArumi@users.noreply.github.com>
2022-12-07 13:10:18 +0100

    Created mockNew for testing purposes

---
+++ src/mocks/mockNew.js
@@ -0,0 +1,17 @@
+const mockNew = {
+  title: "December Press Release",
+  description:
+    "Allfunds reports on the progress and end of its share buyback programme",
+  storageDate: "2022-12-05T20:00:00.000Z",
+  content:
+    "London/Madrid/Amsterdam - Allfunds Group plc (“Allfunds”) (TICKER: ALLFG) informs today that, under its share buyback programme announced on 8 November 2022, 129,188 of its own ordinary shares were repurchased on 28 November 2022 on Euronext Amsterdam. The shares were repurchased at an average price of €6.8936 per share. The total consideration of the repurchase was €890,570.69. The maximum total value of the share buyback programme amounted to €10 million. To date, 1,380,322 ordinary shares have been repurchased for a total consideration of €9,999,993.03. Therefore, the programme has now been completed. The buyback has been implemented pursuant to the authority granted by the shareholders of Allfunds at its annual general meeting held on 21 April 2022 and is executed within the parameters prescribed by Regulation (EU) 596/2014 on market abuse and the safe harbour parameters prescribed by Commission Delegated Regulation (EU) 2016/1052 for share buybacks. For detailed information on the individual share purchase transactions, see the Allfunds investor website at: https://investors.allfunds.com/share_info#share_programme. This press release is issued in connection with the disclosure and reporting obligation set out in Article 2(2) of Commission Delegated Regulation (EU) 2016/1052.",
+  archived: false,
+
+  author: {
+    authorName: "First Author",
+    id: "638c7ab09697e7ea8b97edba",
+  },
+  id: "638c88209697e7ea8b97edd0",
+};
+
+export default mockNew;
