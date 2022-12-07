e83f4f683d8924f8bef83c64bfda01fa85efd301
VictorArumi <99591372+VictorArumi@users.noreply.github.com>
2022-12-08 00:03:49 +0100

    Added default, news and archived news routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import AppStyled from "./AppStyled";
 import Header from "./components/Header/Header";
 import NewsPage from "./pages/NewsPage/NewsPage";
+import { Navigate, Route, Routes } from "react-router-dom";
+import ArchivedNewsPage from "./pages/ArchivedNewsPage/ArchivedNewsPage";
 
 function App() {
   return (
     <AppStyled>
       <Header />
-      <NewsPage />
+      <Routes>
+        <Route path="/" element={<Navigate to="/news" />} />
+        <Route path="/news" element={<NewsPage />} />
+        <Route path="/archived" element={<ArchivedNewsPage />} />
+      </Routes>
     </AppStyled>
   );
 }
