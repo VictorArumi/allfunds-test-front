94670cebe0c1275de26d66b229313647071def7c
VictorArumi <99591372+VictorArumi@users.noreply.github.com>
2022-12-08 18:26:43 +0100

    Added openform to newspage, conditional rendering based on page state

--- src/pages/NewsPage/NewsPage.js
+++ src/pages/NewsPage/NewsPage.js
@@ -1,13 +1,16 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import NewsList from "../../components/NewsList/NewsList";
 import { loadNewsThunk } from "../../redux/thunks/newsThunks";
 import NewsPageStyled from "./NewsPageStyled";
 import { MdOutlineAddCircle } from "react-icons/md";
+import NewForm from "../../components/NewForm/NewForm";
 
 const NewsPage = () => {
   const dispatch = useDispatch();
   const news = useSelector((state) => state.news);
+
+  const [openForm, setOpenForm] = useState(false);
 
   useEffect(() => {
     dispatch(loadNewsThunk());
@@ -18,12 +21,13 @@ const NewsPage = () => {
       <div className="page-header">
         <h2 className="page-header__title">News view</h2>
       </div>
-      <div className="add-new-container">
+      <div className="add-new-container" onClick={() => setOpenForm(!openForm)}>
         <p>Add new</p>
         <div className="icon-container">
           <MdOutlineAddCircle size={28} />
         </div>
       </div>
+      {openForm ? <NewForm setOpenForm={setOpenForm} /> : ""}
       <div className="list-container">
         <NewsList news={news} />
       </div>
